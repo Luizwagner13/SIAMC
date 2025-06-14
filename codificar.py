e020752c7f73837591843e9720854142edd5d975
Luizwagner13 <LWPCDGALO@GMAIL.COM>
2025-06-14 23:24:58 +0100

    Add files via upload

---
+++ codificar.py
@@ -0,0 +1,17 @@
+# codificar.py
+
+def get_codigos_disponiveis():
+    return [
+        '313 codigos',
+        '319 codigos',
+        '321 codigos',
+        '320 (rec) codigos',
+        '300 codigos',
+        '343 codigos',
+        '329 codigos'
+    ]
+
+def processar_codificacao(codigo_selecionado, dados):
+    # Aqui no futuro vamos usar as regras da planilha
+    # Por enquanto é só uma simulação
+    return [f"Você selecionou: {codigo_selecionado}", "Resultado fictício gerado aqui."]
